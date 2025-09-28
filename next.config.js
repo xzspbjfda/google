ac78441b9c09d8d17376fd2c5190fadaa37bad0e
xzspbjfda <422213870@qq.com>
2025-09-28 11:21:06 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // 启用图像优化
+  images: {
+    domains: [], // 如需加载外部图片，添加对应域名
+  },
+  // 配置环境变量（可选）
+  env: {
+    PROXY_TARGET: process.env.PROXY_TARGET || "https://www.google.com/",
+  },
+  // 自定义构建输出目录
+  distDir: 'build',
 }
 
 module.exports = nextConfig
